cda44355908b559e6e45e2d8f2e42a20754bc7f9
mssevov18 <ms.sevov@gmail.com>
2023-03-28 17:42:57 +0300

    Extract postBuild logic to a .bat

---
+++ ClientServer-V2/CommunicationLibrary/postBuild.bat
@@ -0,0 +1,14 @@
+@echo off
+
+set user_folder=m4ss
+
+for /f %%a in ('powershell -command "Write-Output $env:USERPROFILE"') do set user_profile=%%a
+
+if "%user_profile:~-4%"=="%user_folder%" (
+	cd "C:\Users\m4ss\source\repos\mssevov18\ClientServerAPI\ClientServer-V2\CommunicationLibrary"
+	xcopy ".\bin\Release\net6.0\CommunicationLibrary.dll" "..\..\..\..\..\..\Documents\libraries\CommunicationLibrary.dll" /Y
+	xcopy ".\bin\Release\net6.0\CommunicationLibrary.dll" "..\..\..\..\Thesis2023\2223-otj-12-project-repo-csharp-mssevov18\libraries\CommunicationLibrary.dll" /Y
+) else (
+	echo You are not logged into the correct user folder.
+)
+
