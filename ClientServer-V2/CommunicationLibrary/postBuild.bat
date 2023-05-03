93a25911e23387e86224e88bb3481515ba180047
mssevov18 <mssevov18@codingburgas.bg>
2023-05-03 16:13:09 +0300

    Update postBuild script

--- ClientServer-V2/CommunicationLibrary/postBuild.bat
+++ ClientServer-V2/CommunicationLibrary/postBuild.bat
@@ -5,9 +5,9 @@ set user_folder=m4ss
 for /f %%a in ('powershell -command "Write-Output $env:USERPROFILE"') do set user_profile=%%a
 
 if "%user_profile:~-4%"=="%user_folder%" (
-	cd "C:\Users\m4ss\source\repos\Thesis2023\ClientServerAPI\ClientServerAPI\ClientServer-V2\CommunicationLibrary"
-	xcopy ".\bin\Release\net6.0\CommunicationLibrary.dll" "..\..\..\..\..\..\Documents\libraries\CommunicationLibrary.dll" /Y
-	xcopy ".\bin\Release\net6.0\CommunicationLibrary.dll" "..\..\..\..\2223-otj-12-project-repo-csharp-mssevov18\libraries\CommunicationLibrary.dll" /Y
+	cd "C:\Users\m4ss\source\repos\Thesis2023\2223-otj-12-project-repo-csharp-mssevov18\ClientServerAPI\ClientServer-V2\CommunicationLibrary"
+	xcopy ".\bin\Release\net6.0\CommunicationLibrary.dll" "..\..\..\..\..\..\..\Documents\libraries\CommunicationLibrary.dll" /Y
+	xcopy ".\bin\Release\net6.0\CommunicationLibrary.dll" "..\..\..\2223-otj-12-project-repo-csharp-mssevov18\libraries\CommunicationLibrary.dll" /Y
 ) else (
 	echo You are not logged into the correct user folder.
 )
